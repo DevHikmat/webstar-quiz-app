5107dac92a79ac4451470b8167240b43c982e284
DevHikmat <devhikmatgithub@gmail.com>
2024-08-27 20:12:52 +0500

    base url uxlab qoladiganiga o'zgartrildi.

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://quizzapp-server-production.up.railway.app/api",
+  // baseURL: "https://quizzapp-server-production.up.railway.app/api",
+  baseURL: "https://test-app-lw9t.onrender.com/api",
 });
 
 export const setAxiosInstanceToken = (token) => {
